0a3c5d8ba18b7382c3f9ee2ccde13e36214b704c
Giangy25 <angirosace99@gmail.com>
2026-02-21 00:14:42 +0100

    sistemo mappa

--- src/app/componenti/contatti-component/contatti-component.ts
+++ src/app/componenti/contatti-component/contatti-component.ts
@@ -3,37 +3,42 @@ import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-contatti-component',
-  imports: [],
   templateUrl: './contatti-component.html',
-  styleUrl: './contatti-component.css',
+  styleUrls: ['./contatti-component.css'], // corretto
 })
 export class ContattiComponent implements AfterViewInit {
-
   private map: any; 
-  private centroid: [number, number] = [45.47393, 12.23573]
+  private centroid: [number, number] = [45.47393, 12.23573];
 
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
   async ngAfterViewInit(): Promise<void> {
-    // Only run in the browser
     if (isPlatformBrowser(this.platformId)) {
-      const L = await import('leaflet'); // dynamic import
+      const L = await import('leaflet');
 
-      // Initialize map
+      // Fix marker icons
+      delete (L.Icon.Default.prototype as any)._getIconUrl;
+      L.Icon.Default.mergeOptions({
+        iconRetinaUrl: 'assets/leaflet/marker-icon-2x.png',
+        iconUrl: 'assets/leaflet/marker-icon.png',
+        shadowUrl: 'assets/leaflet/marker-shadow.png',
+      });
+
+      // Inizializza la mappa
       this.map = L.map('map', {
         center: this.centroid,
         zoom: 16
       });
 
       // Tiles
-      const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 18,
         minZoom: 10,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        attribution: '&copy; OpenStreetMap contributors'
       }).addTo(this.map);
 
-      // Add 5 jittered markers
-      L.marker([45.47393, 12.23573]).addTo(this.map);
+      // Marker
+      L.marker(this.centroid).addTo(this.map);
     }
   }
 }
